ab7f7d6b3b06f95068caa840d9b38533e4f4f1a8
Lintiin <41469524+lintapop@users.noreply.github.com>
2021-07-11 11:46:54 +0800

    'add_search_form'

--- app.js
+++ app.js
@@ -11,20 +11,17 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 // Routes
-
 app.get('/', (req, res) => {
-    // set the url of the gif
-    const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
-    // render the hello-gif view, passing the gifUrl into the view to be displayed
-    res.render('hello-gif', { gifUrl })
+    console.log(req.query) // => "{ term: hey" }
+    res.render('home')
 })
 
-app.get('/greetings/:name', (req, res) => {
-    // grab the name from the path provided
-    const name = req.params.name;
-    // render the greetings view, passing along the name
-    res.render('greetings', { name });
-})
+// app.get('/greetings/:name', (req, res) => {
+//     // grab the name from the path provided
+//     const name = req.params.name;
+//     // render the greetings view, passing along the name
+//     res.render('greetings', { name });
+// })
 
 // app.get('/', (req, res) => {
 //     res.send('I have set the route in an Express project!');

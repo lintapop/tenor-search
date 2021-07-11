4cdcfdb0ea78ac62b8997f2b349ee7d225309786
Lintiin <41469524+lintapop@users.noreply.github.com>
2021-07-11 08:19:34 +0800

    'adding_middleware-template_engine'

--- app.js
+++ app.js
@@ -5,11 +5,32 @@ const express = require('express');
 const app = express();
 
 // Middleware
+const exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 // Routes
+
 app.get('/', (req, res) => {
-    res.send('I have set the route in an Express project!');
-});
+    // set the url of the gif
+    const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
+    // render the hello-gif view, passing the gifUrl into the view to be displayed
+    res.render('hello-gif', { gifUrl })
+})
+
+app.get('/greetings/:name', (req, res) => {
+    // grab the name from the path provided
+    const name = req.params.name;
+    // render the greetings view, passing along the name
+    res.render('greetings', { name });
+})
+
+// app.get('/', (req, res) => {
+//     res.send('I have set the route in an Express project!');
+// });
+
+
 // Start Server
 
 app.listen(3000, () => {
